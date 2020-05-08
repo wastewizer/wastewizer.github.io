b4e99c15e6685e9ad47dcc439d744d8b2d1d029c
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-05-08 14:42:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Get references to UI elements
-let connectButton = document.getElementById('connect');
-let disconnectButton = document.getElementById('disconnect');
+let connectButton = document.getElementsByClassName('connect');
+let disconnectButton = document.getElementsByClassName('disconnect');
 let terminalContainer = document.getElementById('terminal');
 let sendForm = document.getElementById('send-form');
 let inputField = document.getElementById('input');
