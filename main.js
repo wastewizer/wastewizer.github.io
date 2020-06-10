3fc711c8948256a66fefb56aef044088ef8ff2e5
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-10 17:21:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ var particle = new Particle();
 particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }).then(function(data) {
   log('Login Successful');
   log(data.body.access_token);
-  particle.getEventStream({ deviceId: 'e00fce68a38d68b5d14b3e8b', name: "StringWeight", auth: data.body.access_token }).then(function(stream) {
+  
+  var myDevice = 'e00fce68a38d68b5d14b3e8b';
+  var myToken = data.body.access_token;
+  
+  /*
+  particle.getEventStream({ deviceId: myDevice, name: "StringWeight", auth: myToken }).then(function(stream) {
     stream.on('event', function(feed) {
       log('Get Event Successful');
       log(feed.data);
@@ -15,6 +20,17 @@ particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }
       document.getElementById("weight").innerHTML = 'Hello World!!';
     });
   });
+ */ 
+ 
+ particle.getVariable({ deviceId: myDevice, name: "StringWeight", auth: myToken }).then(function(stream) {
+    // insert code to do something with stream.body.result
+   weight = stream.body.result;
+   log(weight);
+    document.getElementById("weight").innerHTML = "Weight is " + stream.body.result);
+ }, function(err) {
+        console.log("An error occurred retrieving data:", err);
+});
+  
 });
 
 
