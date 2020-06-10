b82ec0160899f73b07b783b37f1e32d71befef2d
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-10 12:45:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ let StatusButton = document.getElementById('status');
 let PickupButton = document.getElementById('pickup');
 let Debug = document.getElementById('debug');
 
-$ bower install particle-api-js
-
 log('Hello World');
 var weight;
 
 particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }).then(function(data) {
+  log('Login Successful');
   log(data);
   particle.getEventStream({ deviceId: 'e00fce68a38d68b5d14b3e8b', name: "StringWeight", auth: data.body.access_token }).then(function(stream) {
     stream.on('event', function(feed) {
       log(feed.data);
       weight = feed.data;
-      document.getElementById("weight").innerHTML = 'Hello World';
+      document.getElementById("weight").innerHTML = 'Hello World!!';
     });
   });
 });
@@ -39,187 +38,3 @@ StatusButton.addEventListener('click', function() {
 PickupButton.addEventListener('click', function() {
   disconnect();
 });
-
-// Launch Bluetooth device chooser and connect to the selected
-function connect() {
-  return (deviceCache ? Promise.resolve(deviceCache) :
-      requestBluetoothDevice()).
-      then(device => connectDeviceAndCacheCharacteristic(device)).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => log(error));
-}
-
-function requestBluetoothDevice() {
-  log('Requesting bluetooth device...');
-
-  return navigator.bluetooth.requestDevice({
-    filters: [{services: [0xFFE0]}],
-  }).
-      then(device => {
-        log('"' + device.name + '" bluetooth device selected');
-        deviceCache = device;
-        document.getElementById("dumpster").innerHTML = device.name;
-    
-        // Added line
-        deviceCache.addEventListener('gattserverdisconnected',
-            handleDisconnection);
-
-        return deviceCache;
-      });
-}
-
-// Connect to the device specified, get service and characteristic
-function connectDeviceAndCacheCharacteristic(device) {
-  if (device.gatt.connected && characteristicCache) {
-    return Promise.resolve(characteristicCache);
-  }
-
-  log('Connecting to GATT server...');
-
-  return device.gatt.connect().
-      then(server => {
-        log('GATT server connected, getting service...');
-
-        return server.getPrimaryService(0xFFE0);
-      }).
-      then(service => {
-        log('Service found, getting characteristic...');
-
-        return service.getCharacteristic(0xFFE1);
-      }).
-      then(characteristic => {
-        log('Characteristic found');
-        characteristicCache = characteristic;
-    
-        return characteristicCache;
-      });
-}
-
-// Enable the characteristic changes notification
-function startNotifications(characteristic) {
-  log('Starting notifications...');
-
-  return characteristic.startNotifications().
-      then(() => {
-        log('Notifications started');
-        // Added line
-        characteristic.addEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
-      });
-}
-
-// Data receiving
-function handleCharacteristicValueChanged(event) {
-  let value = new TextDecoder().decode(event.target.value);
-
-  for (let c of value) {
-    if (c === '\n') {
-      let data = readBuffer.trim();
-      readBuffer = '';
-
-      if (data) {
-        receive(data);
-      }
-    }
-    else {
-      readBuffer += c;
-    }
-  }
-}
-
-// Received data handling
-function receive(data) {
-  document.getElementById("weight").innerHTML = data;
-  log(data,'in');
-  if (maxVal < data) {
-    maxVal = data;
-    document.getElementById("maxWeight").innerHTML = data;
-  }
-}
-
-// Output to terminal
-function log(data, type = '') {
-  terminalContainer.insertAdjacentHTML('beforeend',
-      '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
-}
-
-// Disconnect from the connected device
-function disconnect() {
-  if (deviceCache) {
-    log('Disconnecting from "' + deviceCache.name + '" bluetooth device...');
-    deviceCache.removeEventListener('gattserverdisconnected',
-        handleDisconnection);
-
-    if (deviceCache.gatt.connected) {
-      deviceCache.gatt.disconnect();
-      log('"' + deviceCache.name + '" bluetooth device disconnected');
-    }
-    else {
-      log('"' + deviceCache.name +
-          '" bluetooth device is already disconnected');
-    }
-  }
-
-  // Added condition
-  if (characteristicCache) {
-    characteristicCache.removeEventListener('characteristicvaluechanged',
-        handleCharacteristicValueChanged);
-    characteristicCache = null;
-  }
-
-  deviceCache = null;
-}
-
-function handleDisconnection(event) {
-  let device = event.target;
-
-  log('"' + device.name +
-      '" bluetooth device disconnected, trying to reconnect...');
-
-  connectDeviceAndCacheCharacteristic(device).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => log(error));
-}
-
-
-// Handle form submit event
-sendForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form sending
-  send(inputField.value); // Send text field contents
-  inputField.value = '';  // Zero text field
-  inputField.focus();     // Focus on text field
-});
-
-// Send data to the connected device
-function send(data) {
-  data = String(data);
-
-  if (!data || !characteristicCache) {
-    return;
-  }
-
-  data += '\n';
-
-  if (data.length > 20) {
-    let chunks = data.match(/(.|[\r\n]){1,20}/g);
-
-    writeToCharacteristic(characteristicCache, chunks[0]);
-
-    for (let i = 1; i < chunks.length; i++) {
-      setTimeout(() => {
-        writeToCharacteristic(characteristicCache, chunks[i]);
-      }, i * 100);
-    }
-  }
-  else {
-    writeToCharacteristic(characteristicCache, data);
-  }
-
-  log(data, 'out');
-}
-
-function writeToCharacteristic(characteristic, data) {
-  characteristic.writeValue(new TextEncoder().encode(data));
-}
-
-*/
