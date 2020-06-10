5b46b21d7c2421af95a349550d0aa39738758c36
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-10 16:05:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }
   log(data);
   particle.getEventStream({ deviceId: 'e00fce68a38d68b5d14b3e8b', name: "StringWeight", auth: data.body.access_token }).then(function(stream) {
     stream.on('event', function(feed) {
+      log('Get Event Successful');
       log(feed.data);
       weight = feed.data;
       document.getElementById("weight").innerHTML = 'Hello World!!';
