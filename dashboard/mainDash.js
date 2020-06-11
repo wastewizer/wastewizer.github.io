e21cc0585504848417d86f258e6d478343c4aca1
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-11 13:57:32 -0400

    Update mainDash.js

--- dashboard/mainDash.js
+++ dashboard/mainDash.js
@@ -3,10 +3,11 @@ let Debug = document.getElementById('debugDash');
 
 log("Debug working");
 // Declare global variables
-var weight;
+let weight;
 var particle = new Particle();
 var user = "aclark@wastewizer.com";
 var pass = "Wastewizer.1";
+var myToken;
 
 document.getElementById("weight").innerHTML = 'Hello World!!';
 
@@ -20,24 +21,31 @@ particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }
   log(data.body.access_token);
   
   var myDevice = 'e00fce68a38d68b5d14b3e8b';
-  var myToken = data.body.access_token;
-  
-  /*
-  particle.getEventStream({ deviceId: myDevice, name: "StringWeight", auth: myToken }).then(function(stream) {
-    stream.on('event', function(feed) {
-      log('Get Event Successful');
-      log(feed.data);
-      weight = feed.data;
-      document.getElementById("weight").innerHTML = 'Hello World!!';
-    });
-  });
- */ 
+  myToken = data.body.access_token;
  
- particle.getVariable({ deviceId: myDevice, name: "StringWeight", auth: myToken }).then(function(stream) {
+ particle.getVariable({ deviceId: myDevice, name: "containerWeight", auth: myToken }).then(function(data1) {
     // insert code to do something with stream.body.result
-   weight = stream.body.result;
+   weight = data1.body.result;
    log(weight);
-    document.getElementById("weight").innerHTML = "Weight is " + stream.body.result);
+    document.getElementById("weight").innerHTML = "Weight is " + data1.body.result;
+ }, function(err) {
+        log("An error occurred retrieving data:", err);
+  });
+  
+   particle.getVariable({ deviceId: myDevice, name: "angleX", auth: myToken }).then(function(data2) {
+    // insert code to do something with stream.body.result
+   weight = data2.body.result;
+   log(weight);
+    document.getElementById("weight").innerHTML = "Weight is " + data2.body.result;
+ }, function(err) {
+        log("An error occurred retrieving data:", err);
+  });
+  
+   particle.getVariable({ deviceId: myDevice, name: "angleY", auth: myToken }).then(function(data3) {
+    // insert code to do something with stream.body.result
+   weight = data3.body.result;
+   log(weight);
+    document.getElementById("weight").innerHTML = "Weight is " + data3.body.result;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
@@ -48,10 +56,10 @@ particle.createUser({ username: user, password: pass }).then(function(data)
   var loginPromise = particle.login('example@email.com', 'pass');
     loginPromise.then(
       function(data) {
-        console.log('Login successful! access_token:', data.access_token);
+        log('Login successful! access_token:', data.access_token);
       },
       function(err) {
-        console.log('Login failed:', err);
+        log('Login failed:', err);
       }
     );
 
