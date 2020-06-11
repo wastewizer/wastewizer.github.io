4193ae16e7a090558f869ee8fc4a655b007ff4b2
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-10 21:12:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ let sendForm = document.getElementById('login');
 let inputUsername = document.getElementById('username');
 let inputPassword = document.getElementById('password');
 
+function validateForm() {
+  var user = document.forms["LoginInfo"]["uname"].value;
+  var pass = document.forms["LoginInfo"]["psw"].value;
+  if (user == "") {
+    alert("Username must be filled out");
+    return false;
+  } else if (pass == "") {
+    alert("Password must be filled out");
+    return false;
+  } else if (user == "aclark@wastewizer.com") {
+      if (pass == "Wastewizer.1") {
+        location = "main";
+      } else {
+        alert("Invalid Username/Password Combination");
+      }
+  }
+}
+
 // Handle login form submit event
 sendForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent form sending
@@ -10,4 +28,3 @@ sendForm.addEventListener('submit', function(event) {
   inputPassword.value = '';  // Zero text field
   inputPassword.focus();     // Focus on text field
 });
-
