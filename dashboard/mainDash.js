fd9032cd5c44275b800edbf90a12038f661caba9
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-12 12:07:35 -0400

    Update mainDash.js

--- dashboard/mainDash.js
+++ dashboard/mainDash.js
@@ -1,22 +1,23 @@
 // Get references to UI elements
 let Debug = document.getElementById('debugDash');
 
-log("Debug working!");
+log("Debug working");
+log(CLIENT_ID_1);
 // Declare global variables
-let weight;
+var X;
+var Y;
+var Z;
 var particle = new Particle();
 var user = "aclark@wastewizer.com";
 var pass = "Wastewizer.1";
 var myToken;
-
-document.getElementById("weight").innerHTML = 'Hello World!!';
 
 function log(data) {
   Debug.insertAdjacentHTML('afterend', '<div>' + data + '</div>');
 }
 
 
-particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }).then(function(data) {
+particle.login({ username : user, password : pass }).then(function(data) {
   log('Login Successful');
   log(data.body.access_token);
   
@@ -25,27 +26,27 @@ particle.login({ username : "aclark@wastewizer.com", password : "Wastewizer.1" }
  
  particle.getVariable({ deviceId: myDevice, name: "containerWeight", auth: myToken }).then(function(data1) {
     // insert code to do something with stream.body.result
-   weight = data1.body.result;
-   log(weight);
-    document.getElementById("weight").innerHTML = "Weight is " + data1.body.result;
+   Z = data1.body.result;
+   log("Z: "+Z);
+    document.getElementById("data1").innerHTML = Z;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
    particle.getVariable({ deviceId: myDevice, name: "angleX", auth: myToken }).then(function(data2) {
     // insert code to do something with stream.body.result
-   weight = data2.body.result;
-   log(weight);
-    document.getElementById("weight").innerHTML = "Weight is " + data2.body.result;
+   X = data2.body.result;
+   log("X: "+X);
+    document.getElementById("data2").innerHTML = X;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
    particle.getVariable({ deviceId: myDevice, name: "angleY", auth: myToken }).then(function(data3) {
     // insert code to do something with stream.body.result
-   weight = data3.body.result;
-   log(weight);
-    document.getElementById("weight").innerHTML = "Weight is " + data3.body.result;
+   Y = data3.body.result;
+   log("Y: "+Y);
+    document.getElementById("data3").innerHTML = Y;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
