fa3edf96ccabace5b6eaf46786e7f2c67484b91d
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-15 12:14:07 -0400

    Update mainDash.js

--- dashboard/mainDash.js
+++ dashboard/mainDash.js
@@ -3,9 +3,9 @@ let Debug = document.getElementById('debugDash');
 
 log("Debug working");
 // Declare global variables
-var X;
-var Y;
-var Z;
+var sgStr;
+var angleStr1;
+var angleStr2;
 var particle = new Particle();
 var user = "aclark@wastewizer.com";
 var pass = "Wastewizer.1";
@@ -23,29 +23,29 @@ particle.login({ username : user, password : pass }).then(function(data) {
   var myDevice = 'e00fce68a38d68b5d14b3e8b';
   myToken = data.body.access_token;
  
- particle.getVariable({ deviceId: myDevice, name: "containerWeight", auth: myToken }).then(function(data1) {
+ particle.getVariable({ deviceId: myDevice, name: "sgStr", auth: myToken }).then(function(data1) {
     // insert code to do something with stream.body.result
-   Z = data1.body.result;
-   log("Z: "+Z);
-    document.getElementById("data1").innerHTML = Z;
+   sgStr = data1.body.result;
+   log("sgStr: "+sgStr);
+    document.getElementById("data1").innerHTML = sgStr;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
-   particle.getVariable({ deviceId: myDevice, name: "angleX", auth: myToken }).then(function(data2) {
+   particle.getVariable({ deviceId: myDevice, name: "angleStr1", auth: myToken }).then(function(data2) {
     // insert code to do something with stream.body.result
-   X = data2.body.result;
-   log("X: "+X);
-    document.getElementById("data2").innerHTML = X;
+   angleStr1 = data2.body.result;
+   log("angleStr1: "+angleStr1);
+    document.getElementById("data2").innerHTML = angleStr1;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
-   particle.getVariable({ deviceId: myDevice, name: "angleY", auth: myToken }).then(function(data3) {
+   particle.getVariable({ deviceId: myDevice, name: "angleStr2", auth: myToken }).then(function(data3) {
     // insert code to do something with stream.body.result
-   Y = data3.body.result;
-   log("Y: "+Y);
-    document.getElementById("data3").innerHTML = Y;
+   angleStr2 = data3.body.result;
+   log("angleStr2: "+angleStr2);
+    document.getElementById("data3").innerHTML = angleStr2;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
