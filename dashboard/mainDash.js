316bbf485d2d67ea6fe4f5d669e6b691cd6f7470
wastewizer <64982868+wastewizer@users.noreply.github.com>
2020-06-15 12:21:19 -0400

    Update mainDash.js

--- dashboard/mainDash.js
+++ dashboard/mainDash.js
@@ -23,32 +23,24 @@ particle.login({ username : user, password : pass }).then(function(data) {
   var myDevice = 'e00fce68a38d68b5d14b3e8b';
   myToken = data.body.access_token;
  
- particle.getVariable({ deviceId: myDevice, name: "sgStr", auth: myToken }).then(function(data1) {
+ particle.getVariable({ deviceId: myDevice, name: "strainGaugeReadings", auth: myToken }).then(function(data1) {
     // insert code to do something with stream.body.result
-   sgStr = data1.body.result;
-   log("sgStr: "+sgStr);
-    document.getElementById("data1").innerHTML = sgStr;
+   strainGaugeReadings = data1.body.result;
+   log("strainGaugeReadings: "+strainGaugeReadings);
+    document.getElementById("data1").innerHTML = strainGaugeReadings;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
-   particle.getVariable({ deviceId: myDevice, name: "angleStr1", auth: myToken }).then(function(data2) {
+   particle.getVariable({ deviceId: myDevice, name: "angles1", auth: myToken }).then(function(data2) {
     // insert code to do something with stream.body.result
    angleStr1 = data2.body.result;
-   log("angleStr1: "+angleStr1);
-    document.getElementById("data2").innerHTML = angleStr1;
+   log("angles1: "+angles1);
+    document.getElementById("data2").innerHTML = angles1;
  }, function(err) {
         log("An error occurred retrieving data:", err);
   });
   
-   particle.getVariable({ deviceId: myDevice, name: "angleStr2", auth: myToken }).then(function(data3) {
-    // insert code to do something with stream.body.result
-   angleStr2 = data3.body.result;
-   log("angleStr2: "+angleStr2);
-    document.getElementById("data3").innerHTML = angleStr2;
- }, function(err) {
-        log("An error occurred retrieving data:", err);
-  });
   
 });
 
